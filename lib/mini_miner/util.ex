96c4678aee2ae845440571e6ac1aa83f24438e93
Keshav Chakravarthy <keshav-c@users.noreply.github.com>
2023-08-23 20:05:19 +0530

    Helper functions for urls

--- lib/mini_miner/util.ex
+++ lib/mini_miner/util.ex
@@ -1,6 +1,47 @@
 defmodule MiniMiner.Util do
   require Logger
 
+  @host "https://hackattic.com"
+
+  @doc """
+  Returns the URL to the problem given the access `token`.
+
+  ## Examples
+
+      iex> MiniMiner.Util.prob_url("bacd")
+      "https://hackattic.com/challenges/mini_miner/problem?access_token=bacd"
+  """
+  def prob_url(token) do
+    uri = URI.new!("#{@host}/challenges/mini_miner/problem")
+    query = URI.encode_query(%{access_token: token})
+    uri = URI.append_query(uri, query)
+    URI.to_string(uri)
+  end
+
+  @doc """
+  Returns the URL for posting the solution given the access `token`.
+  For repeat tries, set `playground` to `true`.
+
+  ## Examples
+
+      iex> MiniMiner.Util.solution_url("bacd")
+      "https://hackattic.com/challenges/mini_miner/solve?access_token=bacd"
+
+      iex> MiniMiner.Util.solution_url("bacd", true)
+      "https://hackattic.com/challenges/mini_miner/solve?access_token=bacd&playground=1"
+  """
+  def solution_url(token, playground \\ false) do
+    uri = URI.new!("#{@host}/challenges/mini_miner/solve")
+
+    query =
+      if playground,
+        do: URI.encode_query(%{access_token: token, playground: 1}),
+        else: URI.encode_query(%{access_token: token})
+
+    uri = URI.append_query(uri, query)
+    URI.to_string(uri)
+  end
+
   @spec get_data(String.t()) :: map()
   def get_data(prob_url) do
     {:ok, {{_version, 200, _msg}, _headers, body}} = :httpc.request(:get, {prob_url, []}, [], [])
---
+++ test/mini_miner/util_test.exs
@@ -0,0 +1,4 @@
+defmodule MiniMiner.UtilTest do
+  use ExUnit.Case, async: true
+  doctest MiniMiner.Util
+end
