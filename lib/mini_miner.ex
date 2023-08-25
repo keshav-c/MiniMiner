c81ab7900f64826bfeb4a756a76d39d9141a04bf
Keshav Chakravarthy <keshav-c@users.noreply.github.com>
2023-08-25 12:20:11 +0530

    Update project main api

--- lib/mini_miner.ex
+++ lib/mini_miner.ex
@@ -1,10 +1,8 @@
 defmodule MiniMiner do
   require Logger
 
-  @prob_url "https://hackattic.com/challenges/mini_miner/problem?access_token=8588d0e947f66656"
-  @soln_url "https://hackattic.com/challenges/mini_miner/solve?access_token=8588d0e947f66656&playground=1"
-
   def run do
-    GenServer.cast(MiniMiner.Miner, {:mine, @prob_url, @soln_url})
+    MiniMiner.Miner.fetch_problem()
+    MiniMiner.Miner.mine()
   end
 end
